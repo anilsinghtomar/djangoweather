692605c53f3afb87f484e88f4c626683c9faaa7f
anil singh tomar <anilsinghtomar28@gmail.com>
2020-03-27 19:48:06 +0530

    added

--- lookup/urls.py
+++ lookup/urls.py
@@ -1,10 +1,11 @@
 
+# This is my views.py file
 from django.urls import path
-from  . import views
+from . import views
+
 urlpatterns = [
-    path('',views.home,name = "home"),
-    path('about',views.home, name = "about"),
+    path('', views.home, name="home"),
+    path('about.html', views.about, name="about"),
 
-
-
+       
 ]
